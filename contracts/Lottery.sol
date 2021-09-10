385836a63788c5f069740cd00e624e20ae3c7b72
Harshvardhan Rajdan <harj220@gmail.com>
2021-09-10 08:05:51 +0530

    updated contracts

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,42 +1,44 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >= 0.5.0 <0.9.0;
+pragma solidity >= 0.5 .0 < 0.9 .0;
 
 
-contract Lottery{
+contract Lottery {
     address public manager;
-    address payable [] public participants;
+    address payable[] public participants;
 
 
-constructor(){
-    manager=msg.sender;
-}
+    constructor() public payable {
+        manager = msg.sender;
+    }
 
- receive() external payable{
-    require(msg.value>=1 ether);
-participants.push(payable(msg.sender));
-}
-function getBalance() public view returns(uint){
-    require(msg.sender==manager);
-    return address(this).balance;
-}
-function random() public view returns(uint){
-    return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
-    
-}
+    function receive() external payable {
+        require(msg.value >= 1 ether);
+        participants.push(msg.sender);
+    }
 
-function selectWinner()public  {
-    require(msg.sender==manager);
-    require(participants.length>=3);
-    
-    uint index;
-    address payable winner;
-    index=random()%participants.length;
-    winner=participants[index];
-    
-    winner.transfer(getBalance());
-    participants=new address payable[](0);
-    
-}
+    function getBalance() public view returns(uint) {
+        require(msg.sender == manager);
+        return address(this).balance;
+    }
+
+    function random() public view returns(uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, participants.length)));
+
+    }
+
+    function selectWinner() public {
+        require(msg.sender == manager);
+        require(participants.length >= 3);
+
+        uint index;
+        address payable winner;
+        index = random() % participants.length;
+        winner = participants[index];
+
+        winner.transfer(getBalance());
+        participants = new address payable[](0);
+
+    }
 
 
 }
